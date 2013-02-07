2b7003c4d3f048cd1095c6a7033711c84c1bc049
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2013-02-06 18:43:49 -0800

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+﻿var pfx = ["webkit", "moz", "ms", "o", ""];
+function RunPrefixMethod(obj, method) {
+    var p = 0, m, t;
+    while (p < pfx.length && !obj[m]) {
+        m = method;
+        if (pfx[p] == "") {
+
+            m = m.substr(0, 1).toLowerCase() + m.substr(1);
+
+        }
+        m = pfx[p] + m;
+        t = typeof obj[m];
+        if (t != "undefined") {
+            pfx = [pfx[p]];
+            return (t == "function" ? obj[m]() : obj[m]);
+        }
+        p++;
+    }
+}
+$(function () {
+    var jVideo = document.getElementById('joostVideo');
+    var autoStarted = false;
+    $("#joostVideo").click(function (e) {
+        if (!autoStarted) {
+            RunPrefixMethod(jVideo, "RequestFullScreen");
+            autoStarted = true;
+        }        
+    });
+
+    jVideo.addEventListener("ended", function () {
+        jVideo.play();
+    }, false);
+});
